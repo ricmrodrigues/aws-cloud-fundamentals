21bdefbd588697a117333ff3bed28715c07a0a5b
ricmrodrigues <ricmrodrigues@gmail.com>
2024-04-08 10:37:16 +0100

    added lambdas

---
+++ SimpleLambda/src/SimpleLambda/Functions.cs
@@ -0,0 +1,27 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace SimpleLambda;
+public class Functions
+{
+    /// <summary>
+    /// Default constructor that Lambda will invoke.
+    /// </summary>
+    public Functions()
+    {
+    }
+
+    public string FunctionHandler(Hello request, ILambdaContext context)
+    {
+        context.Logger.LogInformation($"Hello from {request.World}");
+        return request.World;
+    }
+}
+
+
+public class Hello
+{
+    public string World { get; set; } = default;
+}
